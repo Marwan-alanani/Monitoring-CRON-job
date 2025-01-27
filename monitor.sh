fef655baf165ef80084ba42ef011807c1f1a7a33
Marwan <marwan-walid1@hotmail.com>
2025-01-27 17:22:59 +0200

    Fixed upper portion

--- monitor.sh
+++ monitor.sh
@@ -25,14 +25,40 @@ function title()
 
 function annotate()
 {
-	echo -e "${GREEN}=============================================================${NC}"
+	echo -e "${YELLOW}=============================================================${NC}"
 }
+
+date_=$(date)
+echo -e "${BLUE}System Monitoring Report - $date_"
 
 annotate
 
 title "Disk Usage"
 disk_use="$(df --output=source,size,used,avail,pcent,target)"
-echo "$disk_use"
+IFS=$'\n' ;
+read -d '' -r -a y <<<"$disk_use"
+IFS=$' \n\t'
+y_len=${#y[@]}
+
+for (( i=0;i<${y_len};i++ ));
+do
+	row="${y[$i]}"
+	row=($row)
+	if [ $i -eq 0 ];then
+		row[1]="Size"
+		#echo "${row[0]}"
+	fi
+
+	for (( j=0;j<${#row[@]};j++ ));
+	do
+		if [ $i -eq 0 ] && [ $j -eq $(( ${#row[@]} - 1 )) ];then
+			printf "%s " "${row[$j]}"
+			continue
+		fi
+		printf "%-18s" "${row[$j]}"
+	done
+	echo ""
+done
 
 out="$( df --output=pcent )"
 i=-1
@@ -48,16 +74,13 @@ do
 		break
 	fi
 done
-annotate
-echo ""
+echo "";echo ""
 
-annotate
+# i need to fix this
 title "CPU Usage"
 #out=`top`
 echo "$out"
-annotate
-echo ""
-annotate
+echo "";echo ""
 title "Memory Usage"
 out=$(free | awk 'NR==2 {print $2,$3,$4}')
 arr=($out)
@@ -68,4 +91,4 @@ used=$( KbToGb $used )
 free=${arr[2]}
 free=$( KbToGb $free )
 printf "Total Memory:${total}GB\nUsed Memory:${used}GB\nFree Memory:${free}GB\n"
-annotate
+echo "";echo ""
